4ec70139dd45873e5f1dc965e9699776fd09eba8
ExhoAR22 <exhoar@gmail.com>
2021-05-01 02:33:40 +0300

    Created a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="altbuild", # Replace with your own username
+    version="0.0.1",
+    author="ExhoAR22",
+    author_email="exhoar@gmail.com",
+    description="Cross-platform MSBuild alternative",
+    url="https://github.com/ExhoAR22/altbuild",
+    packages=['altbuild']
+)
